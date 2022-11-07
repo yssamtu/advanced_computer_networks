936d01a6e37de5ed56e2a92b6af4c7fffd5c7738
yssamtu <yssamtu@gmail.com>
2022-11-07 12:21:41 +0100

    lab1

--- lab0/network_bridge.py
+++ lab0/network_bridge.py
@@ -27,6 +27,17 @@ class BridgeTopo(Topo):
         Topo.__init__(self)
 
         # TODO: add nodes and links to construct the topology
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
 
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(s1, s2)
+        self.addLink(s2, h3)
+        self.addLink(s2, h4)
 
 topos = {'bridge': (lambda: BridgeTopo())}
--- lab1/learning_switch.py
+++ lab1/learning_switch.py
@@ -1,15 +1,15 @@
 # This code is part of the Advanced Computer Networks (2020) course at Vrije 
 # Universiteit Amsterdam.
 
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not
-# use this file except in compliance with the License. You may obtain a copy
-# of the License at
+# Licensed under the Apache Licence, Version 2.0 (the "Licence"); you may not
+# use this file except in compliance with the Licence. You may obtain a copy
+# of the Licence at
 #   http://www.apache.org/licenses/LICENSE-2.0
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# distributed under the Licence is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
+# Licence for the specific language governing permissions and limitations
+# under the Licence.
 
 
 from ryu.base import app_manager
@@ -28,20 +28,23 @@ class LearningSwitch(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(LearningSwitch, self).__init__(*args, **kwargs)
 
-        # Initialize mac address table
+        # Initialise mac address table
         self.mac_to_port = {}
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
-        
+        # The function which is the setting switch first connects controller
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
         # Initial flow entry for matching misses
+        # Null match which can match the match rule of any packet
         match = parser.OFPMatch()
+        # Send every packet which do not know how to address to controller
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
+        # Set the priority of table-miss flow entry to the lowest (0) in switch
         self.add_flow(datapath, 0, match, actions)
 
     # Add a flow entry to the flow-table
@@ -49,7 +52,7 @@ class LearningSwitch(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        # Construct flow_mod message and send it
+        # Construct flow_mod message and send it to switch
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
         mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
                                 match=match, instructions=inst)
@@ -58,7 +61,6 @@ class LearningSwitch(app_manager.RyuApp):
     # Handle the packet_in event
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
-        
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
@@ -68,12 +70,30 @@ class LearningSwitch(app_manager.RyuApp):
         dpid = datapath.id
 
         # TODO: learning switch implementation
-
+        in_port = msg.match['in_port']
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        dst = eth.dst # the MAC address of destination
+        src = eth.src # the MAC address of source
+        if pkt.protocols[1].protocol_name != 'arp':
+            self.mac_to_port.setdefault(dpid, {})
+            self.mac_to_port[dpid][src] = in_port
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+        actions = [parser.OFPActionOutput(out_port)]
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
 
         # Construct packet_out message and send it
         out = parser.OFPPacketOut(datapath=datapath,
                                   in_port=in_port, 
                                   actions=actions, 
-                                  buffer_id=datapath.ofproto.OFP_NO_BUFFER,
-                                  data=msg.data)
+                                  buffer_id=msg.buffer_id,
+                                  data=data)
         datapath.send_msg(out)
